bb037cb3943869ae149c3dea6234c4db120f8fbe
tabdeelstudios <aquib@tabdeel.in>
2020-01-21 08:49:15 +0530

    v1.0
    Pushed the 2019 backup.

---
+++ events.php
@@ -0,0 +1,471 @@
+<!DOCTYPE html>
+<html class="no-js">
+	<head>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Events Gallery | Uddeshya</title>
+		<meta name="description" content="Uddeshya India | Empowering Youth Fueling Change" />
+		<meta name="keywords" content="uddeshya, youth, awareness, event, impact, one billion rising, minute to win it, uddeshya vit" />
+		<meta property="og:title" content="Events | Uddeshya India"/>
+		<meta property="og:image" content="http://www.uddeshyaindia.org/logo.png" />
+		<meta property="og:url" content="http://events.uddeshyaindia.org"/>
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="UDDESHYA INDIA | Empowering Youth. Fueling Change."/>
+		<meta name="author" content="Deepak Grover" />
+		<link rel="icon" type="image/png"  href="http://www.uddeshyaindia.org/images/favicon.png">
+    	<link rel="shortcut icon"  href="http://www.uddeshyaindia.org/images/favicon.png">
+		<link rel="stylesheet" href="css/normalise.min.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/custom.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.min.css">
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-53088611-1', 'auto');
+		  ga('send', 'pageview');
+		
+		</script>
+		<style type="text/css">
+			h2{
+				margin-left:100px;
+				font-size: 30px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="back_overlay"></div>
+		<div id="loader"></div>
+		<!--<header>
+			Events Gallery | u d d e s h y a |
+			<a style="color:#fff;" href="../index.html">Go back Home</a>
+		</header>-->
+
+<div class="main">
+			<div style="margin: 0px; padding: 0px; z-index:1; position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; background-image: url(images/backnew.jpg);"></div>
+			<div class="overlay">
+				<header>
+							<!--Navigation bar-->
+						<!--
+							<nav class="navbar navbar-expand-lg navbar-dark navbar-trans menubar">
+<a class="navbar-brand" href="#"><img src="images/newlogo.png"></a>
+  <a class="navbar-brand" href="#"><h1>Uddeshya</h1></a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+    <ul class="navbar-nav  mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">Chapters</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">Training</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"href="">Internship</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="">Contact Us</a>
+      </li>
+       </ul>
+       </div>
+       </nav>  -->
+       <nav class="navbar navbar-expand-lg navbar-dark navbar-trans menubar">
+						  <a class="navbar-brand" href="#"><img src="images/newlogo.png"><br><h1>Uddeshya</h1></a>
+
+						  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="navbar-toggler-icon"></span>
+						  </button>
+
+					  <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+					    <ul class="navbar-nav  mt-2 mt-lg-0">
+					      <li class="nav-item active">
+					        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="">About Us</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="">Events</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="">Chapters</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="">Training</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link"href="">Internship</a>
+					      </li>
+					       <li class="nav-item">
+					        <a class="nav-link" href="">Contact Us</a>
+					      </li>
+					       </ul>
+       					</div>
+       				</nav>
+   
+       <!-- navbar ends -->
+
+					<div class="container">
+						<!--<a href="#"><img src="images/newlogo.png"></a>
+						<h1>uddeshya</h1>-->
+					</div>
+				</header>
+				<div class="container landing" style="margin-bottom:0px;">
+					<div class="row">
+					<!--<article class="col-sm-6 clearfix" style="padding-top: 30px">
+					<h2>Empowering Youth. Fueling Change.</h2>
+					<p style="color:#fff;">An organisation that aims to encourage, equip and empower youth, to take lead as ambassadors of change, and work towards bringing about sustainable change.</p>
+				</article>
+				<article class="col-sm-6 clearfix"style="padding-top: 30px">-->
+			<!--	<h2 style="text-align:center">Uddeshya - The change begins here.</h2>-->
+				<!--<iframe style="margin-top:100px; margin-bottom:273px" width="100%" height="315" src="https://www.youtube.com/embed/mt7U-vnLtMU?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>-->
+				<!--<iframe width="100%" height="315" src="https://www.youtube.com/embed/mt7U-vnLtMU?rel=0&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</article>-->
+			<article class="col-sm-12 clearix" style="padding:30px">
+				<h1 style="color:#FFCE55;letter-spacing: 3px;font-family:'trackregular';text-align: center;">Events</h1>
+			</article>
+</div>
+					<!-- <h2>Parliament Direct</h2>
+					<p style="color:#fff;">An initiative to make the Parliament more accessible and responsive to citizens.</p>
+					
+					<a  style="margin-bottom:0px;" target="_blank" href="http://parliamentdirect.uddeshyaindia.org/" taregt="_blank" class="btn-border">Learn More</a> -->
+				</div>
+
+			</div>
+		</div>
+
+		<!-- ends -->
+		<section id="main">
+				
+				<h1 class="title" style="font-size:36px;padding-left:12px;letter-spacing:3px;color:#F26453;font-family:'trackregular'">National Initiatives</h1>
+				<div class="line" style="background-color:black;padding-left:10px"></div>
+
+			<ul>
+				
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/parliamentdirect.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">PARLIAMENT DIRECT</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/pd.jpg">
+					</a>
+				</li>
+				
+				<li class="main_item per_50 "style="padding:10px">
+					<a href="events/obr.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">ONE BILLION RISING</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/obr1.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/kaagaz.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">KAAGAZ</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/kaagaz.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/impact.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">IMPACT</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impactback.jpg">
+					</a>
+				</li>
+
+
+			</ul>
+			
+				<h1 class="title" style="font-size:36px;padding-left:12px;letter-spacing:3px;color:#F26453;font-family:'trackregular'">National Initiatives</h1>
+				<div class="line" style="background-color:black;padding-left:10px"></div>
+			
+
+				
+			<!-- <div>
+				<br/><hr/>
+				<h2 class="title">Public Speaking Activities</h2>
+				<hr/><br/>
+			</div> -->
+
+			<ul>
+				
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/thebigquestion.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">The BIG QUESTION</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/tbq.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/zubaan.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">ZUBAAN</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/zubaan.jpg">
+					</a>
+				</li>
+				
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/hightalks.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">HIGH TALKS</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/hightalks.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/beyondthehigh.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">BEYONG THE HIGH</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/beyondthehigh.jpg">
+					</a>
+				</li>
+			</ul>
+
+			<!-- <hr><div>
+				<br/><hr/>
+				<h2 class="title">Awareness Drives</h2>
+				<hr/><br/>
+			</div> -->
+
+			<ul>
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/freshersinc.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">FRESHER'S INC.</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/freshers.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/genesis.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">GENESIS</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/genesis.jpg">
+					</a>
+				</li>
+				
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/invigorate.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">INVIGORATE</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/invigorate.jpg">
+					</a>
+				</li>
+
+				
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/aidswalk.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">AIDS WALK</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/aidswalknew.jpg">
+					</a>
+				</li>
+
+				
+
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/anandam.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">ANANDHAM</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/anandam.jpg">
+					</a>
+				</li>
+			
+
+			<!-- <hr><div>
+				<br/><hr/>
+				<h2 class="title">Outreach Activities</h2>
+				<hr/><br/>
+			</div> -->
+
+			<!-- <ul>
+				
+				<li class="main_item per_50">
+					<a href="events/clowning.html">
+						<div class="bottom_bar magneto"></div>
+						<div class="roll_overlay magneto"></div>
+						<div class="main_title">
+							<h1 class="title">HUMANITARIAN CLOWNING</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li>
+				
+				<li class="main_item per_50">
+					<a href="events/tribalmentoring.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">TRIBAL MENTORING</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50">
+					<a href="events/freemedicalcamp.html">
+						<div class="bottom_bar purple"></div>
+						<div class="roll_overlay purple"></div>
+						<div class="main_title">
+							<h1 class="title">FREE MEDICAL CAMP</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50">
+					<a href="events/zumba.html">
+						<div class="bottom_bar red"></div>
+						<div class="roll_overlay red"></div>
+						<div class="main_title">
+							<h1 class="title">ZUMBA FITENSS WORKSHOP</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li>
+
+				<li class="main_item per_50">
+					<a href="events/carfreesunday.html">
+						<div class="bottom_bar magneto"></div>
+						<div class="roll_overlay magneto"></div>
+						<div class="main_title">
+							<h1 class="title">CAR FREE SUNDAY</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li> -->
+
+				<!-- <li class="main_item per_50">
+					<a href="events/plantationdrive.html">
+						<div class="bottom_bar blue"></div>
+						<div class="roll_overlay blue"></div>
+						<div class="main_title">
+							<h1 class="title">PLANTATION DRIVE</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li> -->
+
+				<!-- <li class="main_item per_50">
+					<a href="events/skillempowermentevent.html">
+						<div class="bottom_bar purple"></div>
+						<div class="roll_overlay purple"></div>
+						<div class="main_title">
+							<h1 class="title">SKILL EMPOWERMENT EVENT</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/impact.jpg">
+					</a>
+				</li>
+ -->
+				<li class="main_item per_50" style="padding:10px">
+					<a href="events/suicidepreventionday.html">
+						<div class="bottom_bar dark-red"></div>
+						<div class="roll_overlay dark-red"></div>
+						<div class="main_title">
+							<h1 class="title">SUICIDE PREVENTION DAY</h1>
+							<div class="line"></div>
+						</div>
+						<img src="images/suicide.jpg">
+					</a>
+				</li>
+
+			</ul>
+
+		</section>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script type="text/javascript" src="js/custom.js"></script>
+		<script type="text/javascript">
+			$(window).load(function() {
+				$('#loader').fadeOut(1500);
+				$('.back_overlay').fadeOut(1600);
+			});
+			function run() {  
+		    var prev = $("div li.main_item:last-child");
+		    $.unique(prev).each(function(i) {
+		      $(this).delay(i*1000).slideUp(function() {
+		        $(this).prependTo(this.parentNode).slideDown();
+		      });
+		    });}
+			window.setInterval(run,6000);
+		</script>
+         
+	</body>
+</html>
