30dd45e6c9f66c0eb2bf0e42c3484ccada74d3b9
tabdeelstudios <56650931+tabdeelstudios@users.noreply.github.com>
2020-03-16 01:22:38 +0530

    v2.0 index

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>Uddeshya India - Empowering Youth Fueling Change</title>
+
+		<meta name="description" content="Uddeshya India | Empowering Youth Fueling Change" />
+		<meta name="keywords" content="uddeshya, youth, awareness" />
+		<meta property="og:title" content="Uddeshya India"/>
+		<meta property="og:image" content="http://www.uddeshyaindia.org/newlogo.png" />
+		<meta property="og:url" content="http://www.uddeshyaindia.org"/>
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="UDDESHYA INDIA - Empowering Youth Fueling Change"/>
+		<meta name="author" content="Aquib Ajani" />
+
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/font.css">
+		<link rel="stylesheet" href="css/custom.css">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    	
+    	
+    	
+    	<link rel="icon" type="image/png"  href="images/favicon.png">
+    	<link rel="shortcut icon"  href="images/favicon.png">
+    	<script src="js/modernizr.custom.js"></script>
+    	<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-53088611-1', 'auto');
+		  ga('send', 'pageview');
+		
+		</script>
+    </head>
+	<body>
+		<div class="main">
+			<div style="margin: 0px; padding: 0px; z-index:1; position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; background-image: url(images/03.png);"></div>
+			<div class="overlay">
+				<header>
+					<div class="container">
+						<a href="#"><img src="images/newlogo.png"></a>
+						<h1>uddeshya</h1>
+					</div>
+				</header>
+				<div class="container landing">
+					<h2>Empowering Youth. Fueling Change.</h2>
+					<p style="color:#fff;">An organisation that aims to encourage, equip and empower youth, to take lead as ambassadors of change, and work towards bringing about sustainable change.</p>
+					<a href="member.php" class="btn-solid red">JOIN THE CAUSE</a>
+					<a href="ideology.php" class="btn-border">Learn More</a>
+				</div>
+				<div class="container landing">
+					<h2>Parliament Direct</h2>
+					<p style="color:#fff;">An initiative to make the Parliament more accessible and responsive to citizens.</p>
+					
+					<a href="http://parliamentdirect.uddeshyaindia.org/" taregt="_blank" class="btn-border">Learn More</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="info clearfix" id="uip">
+			<article class="col-sm-12 blue">
+				<img src="uiplogowebsite.png" alt="UIP Logo" height="50%"/>
+				<h3>Uddeshya Internship Programme</h3>
+				<p>An excellent opportunity for future change makers to improve their skill set and unleash their creativity.</p>
+				<span><a href="http://bit.ly/uipapplication" class="btn-border" target="_blank">Apply now</a></span>
+			</article>
+		</div>
+
+		<div class="info clearfix">
+			<article class="col-sm-6 dark-red">
+				<i class="icon-heart"></i>
+				<h3>Ideology</h3>
+				<p>Aims to create a platform for future change-makers and provide them an opportunity to implement their ideas that can transform the society.</p>
+				<span><a href="ideology.php" class="btn-border">Learn More</a></span>
+			</article>
+			<article class="col-sm-6 yellow grey-color">
+				<i class="icon-microphone" style="color:#666; border-color:rgba(0,0,0,0.1)"></i>
+				<h3>Events</h3>
+				<p>Empowering the youngsters with knowledge regarding HIV-AIDS, substance abuse, political awareness, peer pressure, health and hygiene.</p>
+				<span><a href="http://events.uddeshyaindia.org" target="_blank" class="btn-border grey-color grey-btn">Events Gallery</a></span>
+			</article>
+			<article class="col-sm-6 green">
+				<i class="icon-article"></i>
+				<h3>News</h3>
+				<p>From Newspaper Headlines to Online Magazines, read all about us.</p>
+				<span><a href="news.php" class="btn-border">Read Articles</a></span>
+			</article>
+			<article class="col-sm-6 blue">
+				<i class="icon-group"></i>
+				<h3>Team</h3>
+				<p>A brigade of youngsters set out to inspire change.</p>
+				<span><a href="team.php" class="btn-border">Meet the Team</a></span>
+			</article>
+		</div>
+		
+		
+
+		<!--<article class="slider">
+			<div class="container">
+				<h2 class="grey">Events Highlights</h2>
+				<div id="cbp-qtrotator" class="cbp-qtrotator">
+					<div class="cbp-qtcontent">
+						
+						<h2>INVIGORATE’16</h2>
+						<img class="hid" src="images/1.jpg" alt="img01" />
+						<blockquote>
+						  <p>On the occasion of World Mental Health day Uddeshya organized INVIGORATE to celebrate life and to highlight the positive aspects of it. We invited speakers from Alcoholics Anonymous to share their life experiences and how better their life was after they successfully quit drinking.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						
+						<h2>WORLD AIDS DAY</h2>
+						<img class="hid" src="images/2.jpg" alt="img02" />
+						<blockquote>
+						  <p>On the occasion of World AIDS Day, Uddeshya-VIT had organized a simple candle lighting ceremony. The motive behind observing this day was to pay homage to the victims of this endemic malady and to create awareness about it.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						<h2>ONE BILLION RISING</h2>
+						<img class="hid" src="images/3.jpg" alt="img03" />
+						<blockquote>
+						  <p>Uddeshya, in its quest to bring about a change in this nation, joins hands with &quot;The One Billion Rising&quot; to spread a word about the violence against women. With the onset of dusk on 14th of February, VIT witnessed the caucus of the fervent youth avidly participating in the event.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						
+						<h2>Peer Educator Training Programme (PETP)</h2>
+						<img class="hid" src="images/4.jpg" alt="img04" />
+						<blockquote>
+						  <p>The members of Uddeshya undergo an extensive Peer Educator Training Program (PETP) which enables them to understand and impart the knowledge that is gained from these modules which are developed on pressing issues like HIV-AIDS, Substance Abuse, Escapism, Sexual Abuse and Discrimination by the members themselves.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						
+						<h2>CAUSE CONQUEST</h2>
+						<img class="hid" src="images/5.jpg" alt="img05" />
+						<blockquote>
+						  <p>The theme of the event runs along the similar lines as the objectives of our organization i.e. creating awareness about various issues of concern to the youth community. The forte for this event would be to adjudicate the speed analytical skills of the teams.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						
+						<h2>THE BIG QUESTION</h2>
+						<img class="hid" src="images/6.jpg" alt="img06" />
+						<blockquote>
+						  <p>Discussions lead to debates which lead to solutions. Solutions are what our country and this world needs. Hence the Big Question – a forum for the youth to talk about issues and present their views on issues like sexual abuse in India, illiteracy, honor killing, mercy killing, etc.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						
+						<h2>FRESHERS INC.</h2>
+						<img class="hid" src="images/7.jpg" alt="img07" />
+						<blockquote>
+						  <p>Freshers INC is a life skills orientation program that is conducted annually by Uddeshya in colleges for First year students. The event aims at empowering the youngsters with knowledge so that they make sound choices during their college life as many experience complete freedom for the first time.</p>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						
+						<h2>IMPACT13</h2>
+						<img class="hid" src="images/8.jpg" alt="img05" />
+						<blockquote>
+						  <p>IMPACT 2013 was a national level campaign aimed at youth empowerment, social awakening and sustainable development. Organized by Uddeshya in association with Oikos Vellore, IMPACT 2013 served as the official curtain raiser to graVITas 2013, the annual technical fest of VIT University.</p>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		</article>-->
+
+		<!--<section id="chapterhighlights">
+		<h2 class="grey" style="margin-bottom:20px;">Chapter Highlights</h2>
+		<ul class="nav nav-tabs nav-justified">
+		  <li class="active"><a data-toggle="tab" href="#home"><h4 style="color:#d33b49;">Vellore Chapter</h4></a></li>
+		  <li><a data-toggle="tab" href="#menu1"><h4 style="color:#d33b49;">Mumbai Chapter</h4></a></li>
+		  <li><a data-toggle="tab" href="#menu2"><h4 style="color:#d33b49;">Chennai Chapter</h4></a></li>
+		</ul>
+		
+		<div class="tab-content">
+		  <div id="home" class="tab-pane fade in active">
+		    
+		  </div>
+		  <div id="menu1" class="tab-pane fade">
+		    
+		    <p>Some content in menu 1.</p>
+		  </div>
+		  <div id="menu2" class="tab-pane fade">
+		    
+		    <p>Some content in menu 2.</p>
+		  </div>
+		</div>
+		</section>-->
+
+
+
+
+
+
+
+		<!--<div class="subscribe">
+			<div class="container">
+				<p class="red-color" id="result">Subscribe to our newsletter and get monthly updates straight into your mailbox.</p>
+				<form id="subscribeForm" method="POST" action="process.php">
+					<input type="email" required name="newsMail" placeholder="Enter your email address" />
+					<button class="dark-red" type="submit">Subscribe</button>
+				</form>
+			</div>
+		</div>
+
+		<div class="join">
+			<div class="clearfix">
+				<div class="col-sm-6 join-wrap">
+					<div class="yellow join-link">
+						<h2 class="dark-color">Become a Member</h2>
+						<p class="dark-color">Join the family</p>
+						<a href="member.php" class="btn-border grey-color grey-btn">Sign up</a>
+					</div>
+				</div>
+				<div class="col-sm-6 join-wrap">
+					<div class="dark-red join-link">
+						<h2>Start a Chapter</h2>
+						<p>Involve at a greater level</p>
+						<a href="chapter.php" class="btn-border">Apply Here</a>
+					</div>
+				</div>
+			</div>
+		</div>-->
+		
+		
+		
+		
+
+<section id="fbpage">
+<h2 class="grey" style="margin-bottom:20px;">Latest from Social Media</h2>
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-5">
+<h4 style="color:#d33b49;">Facebook</h4>
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIn.Uddeshya%2F&tabs=timeline&width=320&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="500" style="border:none;overflow:scroll" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+</div>
+<div class="col-sm-5">
+<h4 style="color:#d33b49;">Instagram</h4>
+<!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/a3941fc95d1b54189c24e55685ae7dab.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+</div>
+<div class="col-sm-1"></div>
+</div>		
+</section>
+		
+		
+		
+		
+
+		<div class="contact blue clearfix">
+			<div class="container">
+				<h2>CONTACT US</h2>
+
+				<p id="data">Drop us a line and we will get back to you soon.</p>
+				<form method="POST" id="contactForm" action="contactprocess.php">
+					<input type="text" name="name" required placeholder="&#9733; Name"/>
+					<input type="email" name="email" required placeholder="&#9993; E-Mail Address"/>
+					<textarea name="message" required placeholder="&#9998; Message" rows="5"
+					cols="21"></textarea>
+					<button type="submit" id="sendButton">SEND</button>
+				</form>
+
+				<span class="social">
+					<a href="https://www.facebook.com/in.uddeshya"><i class="icon-facebook"></i></a>
+					<a href="https://www.twitter.com/uddeshyaind"><i class="icon-twitter"></i></a>
+					<!-- <a href="#"><i class="icon-youtube"></i></a>
+					<a href="#"><i class="icon-instagram"></i></a> -->
+				</span>
+
+				<p>Designed by - <a href="https://twitter.com/thewebunagi" target="_blank" style="color:#FFCE55">Deepak Grover</a></p>
+				<p>Managed by - <a href="https://facebook.com/aquib17" target="_blank" style="color:#FFCE55">Aquib Ajani</a></p>
+			</div>
+		</div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/jquery.cbpQTRotator.min.js"></script>
+		<script type="text/javascript">
+		  $(function() {
+			$('#cbp-qtrotator').cbpQTRotator();
+			$("#subscribeForm").submit(function(e)
+			{
+			    var postData = $(this).serializeArray();
+			    var formURL = $(this).attr("action");
+			    $.ajax({
+					type: "POST",
+					url: formURL,
+					data: postData,
+					cache: false,
+					success: function(result){
+						$("#subscribeForm")[0].reset();
+						$("#result").html("You've been successfully subscribed to our newsletter!");
+					}
+				});
+			    e.preventDefault();
+			});
+
+			$("#contactForm").submit(function(e)
+			{
+			    var postData = $(this).serializeArray();
+			    var formURL = $(this).attr("action");
+			    $.ajax({
+					type: "POST",
+					url: formURL,
+					data: postData,
+					cache: false,
+					success: function(result){
+						$("#contactForm")[0].reset();
+						$("#data").html("Your message has been sent!").css("color", "#FFCE55");
+					}
+				});
+			    e.preventDefault();
+			});
+
+		});
+		</script>
+	</body>
+</html>
